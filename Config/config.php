37fed12fbd68ac749bc152e856be8bd742e92e2d
ChenLiu0921 <37284268+ChenLiu0921@users.noreply.github.com>
2019-04-04 21:46:14 +0800

    Add files via upload

---
+++ Config/config.php
@@ -0,0 +1,18 @@
+<?php
+
+//Database config
+
+define('DB_HOST','localhost');
+
+define('DB_USER','root');
+
+define('DB_PASSWORD','c7f96be48b48b1b5');
+
+define('DB_NAME','infs7202');
+
+
+define('ROOT',str_replace('\\','/',dirname(dirname(__FILE__))));
+
+
+
+?>
